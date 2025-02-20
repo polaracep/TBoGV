a2d420d6991fab0658ee3511555176bfbc59e238
Tygrnasrome <pskop.k10@gmail.com>
2025-02-20 20:00:25 +0100

    minigameKomisionalky random difficulties

--- Core/UI/InGameMenu/InGameMenuDeath.cs
+++ Core/UI/InGameMenu/InGameMenuDeath.cs
@@ -136,7 +136,7 @@ internal class InGameMenuDeath : InGameMenu
 	public void OpenMenu()
 	{
 		GenerateDeathMessage();
-		minigame = new MinigameKomisionalky(() => OnSuccess(), () => OnFailure(), -4);
+		minigame = new MinigameKomisionalky(() => OnSuccess(), () => OnFailure(), new Random().Next(-10,5));
 		minigameCompleted = false;
 		Active = true;
 	}
