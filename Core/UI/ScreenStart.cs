3c8b4df56f3b0c3f138122711801f486aac4e28f
polaracep <adam@beyer.cz>
2025-02-26 12:22:11 +0100

    Lil updates and screenstart

---
+++ Core/UI/ScreenStart.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TBoGV;
+
+class ScreenStart : Screen
+{
+    public override void BeginRun(GraphicsDeviceManager graphics)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Draw(SpriteBatch _spriteBatch, GraphicsDeviceManager graphics)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(GameTime gameTime, GraphicsDeviceManager graphics)
+    {
+        throw new System.NotImplementedException();
+    }
+}
