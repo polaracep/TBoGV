bebf3f135332a3cd46270d59bb7a78f16dbdd95f
Pavel Škop <pskop.k10@gmail.com>
2025-03-10 18:32:20 +0100

    fixed highlighted segment

--- Core/Minigame/MinigameKomisionalky.cs
+++ Core/Minigame/MinigameKomisionalky.cs
@@ -65,7 +65,7 @@ public class MinigameKomisionalky : Minigame
 	private void GenerateGreenZone(int difficulty)
 	{
 		int maxSegments = BarWidth / SegmentWidth;
-		int greenSegments = Math.Max(3, difficulty * maxSegments / 10);
+		int greenSegments = Math.Max(3, difficulty + 5);
 		int startSegment = new Random().Next(0, maxSegments - greenSegments);
 
 		greenStart = startSegment * SegmentWidth;
@@ -129,13 +129,13 @@ public class MinigameKomisionalky : Minigame
 			if (arrowPosition + ArrowWidth / 2 >= greenStart && arrowPosition <= greenEnd - ArrowWidth / 2)
 			{
 				successCount++;
-				highlightedSegment = (arrowPosition / SegmentWidth) * SegmentWidth;
+                highlightedSegment = ((arrowPosition + ArrowWidth / 2) / SegmentWidth) * SegmentWidth; 
 				highlightSegment = true;
 				highlightTime = 0.4;
 			}
 			else
 			{
-				highlightedSegment = (arrowPosition / SegmentWidth) * SegmentWidth;
+				highlightedSegment = ((arrowPosition + ArrowWidth / 2) / SegmentWidth) * SegmentWidth;
 				highlightSegment = true;
 				highlightTime = 0.4;
 				State = minigameState.FAILURE;
