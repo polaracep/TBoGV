ad02bfde8ae6ff2994112f0f64c41d227622a5cd
Pavel Škop <pskop.k10@gmail.com>
2025-03-12 17:34:28 +0100

    calculator rarity fix

--- Core/Entity/Item/Containerable/Basic/ItemCalculator.cs
+++ Core/Entity/Item/Containerable/Basic/ItemCalculator.cs
@@ -10,12 +10,12 @@ class ItemCalculator : ItemContainerable
     static Texture2D Sprite;
     public ItemCalculator(Vector2 position)
     {
-		Rarity = 2;
+		Rarity = 3;
 		Position = position;
         Size = new Vector2(50, 50);
         Name = "Kalkulačka";
         Description = "Rychlá jako schovánkovo opravování testů";
-        Stats = new Dictionary<StatTypes, float>() { { StatTypes.DAMAGE, 4 } };
+        Stats = new Dictionary<StatTypes, float>() { { StatTypes.DAMAGE, 5 } };
         Effects = new List<EffectTypes>();
         Sprite = TextureManager.GetTexture("calculator");
         ItemType = ItemTypes.BASIC;
