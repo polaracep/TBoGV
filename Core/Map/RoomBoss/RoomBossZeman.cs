19193bce699096fd073a6df0131525e334175b79
Pavel Škop <pskop.k10@gmail.com>
2025-03-20 21:19:14 +0100

    zeman room prep

--- Core/Map/RoomBoss/RoomBossZeman.cs
+++ Core/Map/RoomBoss/RoomBossZeman.cs
@@ -4,7 +4,7 @@ namespace TBoGV;
 
 public class RoomBossZeman : RoomBoss
 {
-	protected static Vector2 dimensions = new Vector2(10);
+	protected static Vector2 dimensions = new Vector2(18);
 	public RoomBossZeman(Player p) : base(dimensions, p) { }
 
 	protected override EnemyBoss Boss { get; set; } = new BossZeman();
