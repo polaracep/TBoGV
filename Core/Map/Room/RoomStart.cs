faeeea9fdc89994f9e87daff05421c2d1383620e
polaracep <adam@beyer.cz>
2025-03-21 09:34:16 +0100

    nicer startroom

--- Core/Map/Room/RoomStart.cs
+++ Core/Map/Room/RoomStart.cs
@@ -2,17 +2,16 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using TBoGV;
 
-public class RoomStart : Room, IDraw
+public class RoomStart : RoomHallway
 {
     public RoomStart(Vector2 dim, Player p) : base(dim, p) { }
-    public RoomStart(Player p) : base((7, 9, 12), p) { }
+    public RoomStart(Player p) : base(new Vector2(9, 12), p) { }
 
     protected override List<Enemy> validEnemies { get; set; } = [];
 
     public override void Generate()
     {
-        base.GenerateBase(FloorTypes.BASIC, WallTypes.BASIC, DoorTypes.BASIC);
-        GenerateEnemies();
+        base.Generate();
 
 #if DEBUG
         AddDecoTile(this.Dimensions / 2, new TileHeal());
